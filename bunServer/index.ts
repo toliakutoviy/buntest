da33e627b4f52b7fc14c3d09b66398b4bac33653
anatolii nobody <toliakutoviy@gmail.com>
2023-03-06 12:45:32 +0200

    fix

--- bunServer/index.ts
+++ bunServer/index.ts
@@ -3,7 +3,7 @@ const metricName = 'my_custom_super_name'
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        if (req.url === `http://localhost:3000/metrics`) {
+        if (req.url.includes(`/metrics`)) {
             // Return all metrics the Prometheus exposition format
             return new Response(`${metricName}{app="example-nodejs-app"} ${counter}`);
         } else if(req.url !== 'http://localhost:3000/favicon.ico'){
